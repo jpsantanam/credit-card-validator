8991be676d1f64a3b374049a70df1cb7b75fa9ce
jpsantanam <jpsantanamarques2905@gmail.com>
2023-10-09 18:23:16 -0300

    Refactor date validation

--- backend/routes/validator.js
+++ backend/routes/validator.js
@@ -4,14 +4,19 @@ let router = express.Router();
 router.route('/').post(async (req, res) => {
   try {
     const params = req.body;
-    const now = new Date();
-    const expirationDate = new Date(params.date);
-    const isDateValid = expirationDate > now;
+    const isDateValid = validateDate(params.date);
 
     res.status(201).send({ isDateValid });
   } catch (err) {
     res.status(500).send(err);
   }
 });
+
+const validateDate = (date) => {
+  const now = new Date();
+  const expirationDate = new Date(date);
+  const isDateValid = expirationDate > now;
+  return isDateValid;
+};
 
 module.exports = router;
