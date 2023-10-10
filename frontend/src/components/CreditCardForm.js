d21f53f8092f51bfdaa227319b7a90c011f1db1e
jpsantanam <jpsantanamarques2905@gmail.com>
2023-10-09 23:06:56 -0300

    Add validation styling

--- frontend/src/components/CreditCardForm.js
+++ frontend/src/components/CreditCardForm.js
@@ -1,6 +1,12 @@
+import React from 'react';
 import './CreditCardForm.css';
 
-function CreditCardForm({ setReminderData }) {
+function CreditCardForm() {
+  const [isCardNumberValid, setIsCardNumberValid] = React.useState(true);
+  const [isCvvValid, setIsCvvValid] = React.useState(true);
+  const [isDateValid, setIsDateValid] = React.useState(true);
+  const [validationText, setValidationText] = React.useState('');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -13,63 +19,80 @@ function CreditCardForm({ setReminderData }) {
       date: `${formJson.year}/${formJson.month}`,
     };
 
-    await fetch('/card-validator', {
+    const response = await fetch('/card-validator', {
       method: form.method,
       body: JSON.stringify(requestBody),
       headers: { 'Content-Type': 'application/json' },
     });
+
+    const validation = await response.json();
+    setIsCardNumberValid( validation.isCardNumberValid && validation.isLastDigitValid);
+    setIsCvvValid(validation.isCvvValid);
+    setIsDateValid(validation.isDateValid);
+
+    if (validation.isCardNumberValid && validation.isCvvValid && validation.isDateValid && validation.isLastDigitValid) {
+      setValidationText('Valid credit card ✔️');
+    } else {
+      setValidationText('Invalid credit card ❌');
+    }
   };
 
   return (
     <section>
       <h2>Credit Card Validator</h2>
       <form method='POST' onSubmit={handleSubmit}>
-        <label className='Add-label'>
-          Card Number
+        <label className='Form-label'>
+          <span className='Form-span'>Card Number</span>
           <input
             type='number'
             name='cardNumber'
             min='0'
             placeholder='1234 5678 3456 7890'
+            className={`Form-input-width Form-input ${
+              isCardNumberValid ? '' : 'Form-invalid-input'
+            }`}
             required
           />
         </label>
-        <label className='Add-label'>
-          Expiration Month
+        <label className='Form-label'>
+          <span className='Form-span'>Expiration Month</span>
           <input
             type='number'
             name='month'
             placeholder='mm'
             min='1'
             max='12'
+            className={`Form-input ${isDateValid ? '' : 'Form-invalid-input'}`}
             required
           ></input>
         </label>
-        <label className='Add-label'>
-          Expiration Year
+        <label className='Form-label'>
+          <span className='Form-span'>Expiration Year</span>
           <input
             type='number'
             name='year'
             placeholder='yyyy'
             min='1'
+            className={`Form-input ${isDateValid ? '' : 'Form-invalid-input'}`}
             required
           ></input>
         </label>
-        <label className='Add-label'>
-          CVV
+        <label className='Form-label'>
+          <span className='Form-span'>CVV</span>
           <input
             type='number'
             name='cvv'
             placeholder='cvv'
             min='0'
-            className='Add-input'
+            className={`Form-input ${isCvvValid ? '' : 'Form-invalid-input'}`}
             required
           ></input>
         </label>
-        <button type='submit' className='Add-button'>
+        <button type='submit' className='Form-button'>
           Validate
         </button>
       </form>
+      <p className=''>{validationText}</p>
     </section>
   );
 }
